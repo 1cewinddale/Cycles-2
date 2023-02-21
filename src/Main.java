8bb19752e3ff0bde716b94aa83453842d5cc2665
Julia <lplagiatl@gmail.com>
2023-02-21 18:42:15 +0300

    циклы завершение

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         task4();
         task5();
         task6();
-    }
-
-    public static void task1() {
+        task7();
+        task8();
+    }public static void task1() {
         System.out.println("Задача 1");
         int postpone = 15_000;
         int total = 0;
@@ -18,9 +18,7 @@ public class Main {
             total = total + postpone;
             System.out.println("Месяц " + month + " Сумма накоплений равна " + total);
         }
-    }
-
-    public static void task2() {
+    }public static void task2() {
         System.out.println("Задача 2");
         int total = 0;
         while (total < 10) {
@@ -33,9 +31,7 @@ public class Main {
             System.out.print(" " + i);
         }
         System.out.println();
-    }
-
-    public static void task3() {
+    }public static void task3() {
         System.out.println("Задача 3");
         int population = 12_000_000;
         int fertility = 17;
@@ -44,9 +40,7 @@ public class Main {
             population = population + (population / 1000 * fertility) - (population / 1000 * mortality);
             System.out.println("Год " + year + " Численность населения составляет " + population);
         }
-    }
-
-    public static void task4() {
+    }public static void task4() {
         System.out.println("Задача 4");
         int month = 0;
         int postpone = 15000;
@@ -55,9 +49,7 @@ public class Main {
             month++;
         }
         System.out.println("Месяц " + month + " Сумма накоплений равна " + postpone);
-    }
-
-    public static void task5() {
+    }public static void task5() {
         System.out.println("Задача 5");
         int month = 0;
         int postpone = 15000;
@@ -69,22 +61,36 @@ public class Main {
             }
         }
         System.out.println(postpone);
-    }
-
-    public static void task6() {
+    }public static void task6() {
         System.out.println("Задача 6");
         double postpone = 15000;
-        double endPostpone = 0;
         int month = 0;
         for (; month < 108; month++) {
-            postpone = postpone + postpone *0.07;
+            postpone = postpone + postpone * 7 / 100;
             if (month % 6 == 0) {
                 System.out.println("Месяц " + month + " Итого " + postpone);
             }
 
         }
 
+    }public static void task7() {
+        System.out.println("Задача 7");
+        int friday = 5;
+        while (friday <= 31) {
+            friday += 7;
+            System.out.println("Сегодня пятница " + friday + "е число. Нужно подготовить отчет.");
+        }
+
+    }public static void task8() {
+        System.out.println("Задача 8");
+        int cometCooldown = 79;
+        int year = 2023;
+        int cometReturn = 0;
+        while (cometReturn <= year) {
+            cometReturn = cometReturn + cometCooldown;
+            if (year - 200 < cometReturn && cometReturn <= year+100) {
+                System.out.println(cometReturn);
+            }
+        }
     }
 }
-
-
